4748c8b17357ca169337303caa76ca3669706615
nirzaf <mfmfazrin1986@gmail.com>
2022-03-10 12:25:47 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.CompilerServices;
+
+public interface IVehicle
+{
+    void Move();
+}
+
+public interface ICar
+{
+    void Move();
+}
+
+
+public class Audi : IVehicle, ICar
+{
+    void IVehicle.Move()
+    {
+        throw new NotImplementedException();
+    }
+
+    void ICar.Move()
+    {
+        throw new NotImplementedException();
+    }
+}
+
